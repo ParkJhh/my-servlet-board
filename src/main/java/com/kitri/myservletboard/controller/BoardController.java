a9687e1cdea7af81ce153d3f26227597cc440399
ParkJhh <qkrwnsgur145@gmail.com>
2024-01-09 13:12:11 +0900

    JDBC DB연동 및 게시글 리스트 조회 기능 구현

--- src/main/java/com/kitri/myservletboard/controller/BoardController.java
+++ src/main/java/com/kitri/myservletboard/controller/BoardController.java
@@ -1,8 +1,6 @@
 package com.kitri.myservletboard.controller;
-
 import com.kitri.myservletboard.data.Board;
 import com.kitri.myservletboard.service.BoardService;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,12 +11,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-
 @WebServlet("/board/*")
 public class BoardController extends HttpServlet {
-
     BoardService boardService = BoardService.getInstance();
-
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=UTF-8");
@@ -27,12 +22,10 @@ public class BoardController extends HttpServlet {
 
         //URL을 파싱해서 어떤 요청인지 파악
         out.println(req.getRequestURI());
-
         req.setCharacterEncoding("UTF-8");
         String reqURI = req.getRequestURI(); //   /board/list
         String contextPath = req.getContextPath();
         String command = reqURI.substring(contextPath.length());
-
         String view = "/view/board/";
 
         if (command.equals("/board/list")) {
@@ -42,10 +35,9 @@ public class BoardController extends HttpServlet {
             //jsp에게 동적으로 넘겨주어야 한다.
             ArrayList<Board> boards = boardService.getBoards();
             req.setAttribute("boards",boards);
-
             view += "list.jsp";
-        } else if (command.equals("/board/createForm")) {
 
+        } else if (command.equals("/board/createForm")) {
             view += "createForm.jsp";
         } else if (command.equals("/board/create")) {
             //데이터를 읽으니 문자 깨짐이 발생
@@ -53,10 +45,8 @@ public class BoardController extends HttpServlet {
             String title = req.getParameter("title");
             String content = req.getParameter("content");
             String writer = req.getParameter("writer");
-
             Board board = new Board(null, title, content, writer, LocalDateTime.now(), 0,0);
             boardService.addBoard(board);
-
             resp.sendRedirect("/board/list");
             return;
 
@@ -70,6 +60,7 @@ public class BoardController extends HttpServlet {
 
         } else if (command.equals("/board/update")) {
             //게시판 번호에 맞는 글 수정
+            resp.sendRedirect("/view/member/join.html");
             Long id = Long.parseLong(req.getParameter("id"));
             String title = req.getParameter("title");
             String content = req.getParameter("content");
@@ -81,6 +72,8 @@ public class BoardController extends HttpServlet {
             return;
 
         } else if (command.equals("/board/delete")) {
+            //게시판 번호에 맞는 글 삭제
+            resp.sendRedirect("/view/member/login.html");
             Long id = Long.parseLong(req.getParameter("id"));
             Board board = new Board(id, null, null, null, null, 0,0);
 
@@ -91,14 +84,14 @@ public class BoardController extends HttpServlet {
         } else if (command.contains("/board/detail")) {
             Long id = Long.parseLong(req.getParameter("id"));
             Board board = boardService.getBoard(id);
-
             req.setAttribute("board",board);
             view += "detail.jsp";
         }
 
         //페이지 응답하는 방법
-            //리다이렉트 >> 클라이언트에게 새로운 URL을 응답
-            //포워드 >> 요청에 따른 처리결과를 담아 응답
+        //리다이렉트 >> 클라이언트에게 새로운 URL을 응답
+        //포워드
+        //포워드 >> 요청에 따른 처리결과를 담아 응답
         RequestDispatcher dispatcher = req.getRequestDispatcher(view);
         dispatcher.forward(req, resp);
     }
--- src/main/java/com/kitri/myservletboard/data/Board.java
+++ src/main/java/com/kitri/myservletboard/data/Board.java
@@ -3,6 +3,7 @@ package com.kitri.myservletboard.data;
 import java.time.LocalDateTime;
 
 public class Board {
+
     private Long id;
     private String title;
     private String content;
@@ -12,6 +13,7 @@ public class Board {
     private int commentCount;
 
     public Board(){
+
     }
 
     public Board(Long id, String title, String content, String writer, LocalDateTime createdAt, int viewCount, int commentCount) {
@@ -27,42 +29,55 @@ public class Board {
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getContent() {
         return content;
     }
+
     public void setContent(String content) {
         this.content = content;
     }
+
     public String getWriter() {
         return writer;
     }
+
     public void setWriter(String writer) {
         this.writer = writer;
     }
+
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
+
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
+
     public int getViewCount() {
         return viewCount;
     }
+
     public void setViewCount(int viewCount) {
         this.viewCount = viewCount;
     }
+
     public int getCommentCount() {
         return commentCount;
     }
+
     public void setCommentCount(int commentCount) {
         this.commentCount = commentCount;
     }
